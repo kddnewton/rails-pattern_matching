200429bae36ffb2e7a1a844b8a01fc716d64c086
Kevin Newton <kddnewton@gmail.com>
2022-12-19 07:31:55 -0500

    Handle splat pattern matching

--- lib/rails/pattern_matching.rb
+++ lib/rails/pattern_matching.rb
@@ -47,20 +47,35 @@ class ActiveRecord::Base
   #   greeting_for(person) # => "Welcome, stranger!"
   #
   def deconstruct_keys(keys)
-    keys.each_with_object({}) do |key, deconstructed|
-      method = key.to_s
+    if keys
+      # If we've been given keys, then we're going to filter down to just the
+      # values that are being requested.
+      keys.each_with_object({}) do |key, deconstructed|
+        method = key.to_s
 
-      if attribute_method?(method)
-        # Here we're pattern matching against an attribute method. We're
-        # going to use the [] method so that we either get the value or
-        # raise an error for a missing attribute in case it wasn't loaded.
-        deconstructed[key] = public_send(method)
-      elsif self.class.reflect_on_association(method)
-        # Here we're going to pattern match against an association. We're
-        # going to use the main interface for that association which can
-        # be further pattern matched later.
-        deconstructed[key] = public_send(method)
+        # We don't need to worry about the case where the user provided a key
+        # that doesn't match an attribute or association, because the match
+        # will fail by virtue of there not being a key in the result hash.
+        if attribute_method?(method)
+          # Here we're pattern matching against an attribute method. We're
+          # going to use the [] method so that we either get the value or
+          # raise an error for a missing attribute in case it wasn't loaded.
+          deconstructed[key] = public_send(method)
+        elsif self.class.reflect_on_association(method)
+          # Here we're going to pattern match against an association. We're
+          # going to use the main interface for that association which can
+          # be further pattern matched later.
+          deconstructed[key] = public_send(method)
+        end
       end
+    else
+      # If we haven't been given keys, then the user wants to grab up all of the
+      # attributes and associations for this record.
+      attributes.transform_keys(&:to_sym).merge!(
+        self.class.reflect_on_all_associations.to_h do |reflection|
+          [reflection.name, public_send(reflection.name)]
+        end
+      )
     end
   end
 end
@@ -128,12 +143,22 @@ module ActiveModel::AttributeMethods
   #   greeting_for(person) # => "Welcome, stranger!"
   #
   def deconstruct_keys(keys)
-    keys.each_with_object({}) do |key, deconstructed|
-      string_key = key.to_s
+    if keys
+      # If we've been given keys, then we're going to filter down to just the
+      # attributes that were given for this object.
+      keys.each_with_object({}) do |key, deconstructed|
+        string_key = key.to_s
 
-      if attribute_method?(string_key)
-        deconstructed[key] = public_send(string_key)
+        # If the user provided a key that doesn't match an attribute, then we
+        # do not add it to the result hash, and the match will fail.
+        if attribute_method?(string_key)
+          deconstructed[key] = public_send(string_key)
+        end
       end
+    else
+      # If we haven't been given keys, then the user wants to grab up all of the
+      # attributes for this object.
+      attributes.transform_keys(&:to_sym)
     end
   end
 end
--- test/pattern_matching_test.rb
+++ test/pattern_matching_test.rb
@@ -10,10 +10,27 @@ class PatternMatchingTest < Test::Unit::TestCase
     assert_equal({ name: "Rails" }, person.deconstruct_keys([:name]))
   end
 
+  test "model **" do
+    person = Person.new
+    person.name = "Rails"
+
+    assert_equal({ name: "Rails" }, person.deconstruct_keys(nil))
+  end
+
   test "record" do
     post = Post.new(title: "Rails")
 
     assert_equal({ title: "Rails" }, post.deconstruct_keys([:title]))
+  end
+
+  test "record **" do
+    post = Post.create!(title: "Rails")
+    comment = post.comments.build(body: "This is a comment on Rails.")
+
+    assert_equal(
+      { id: 1, title: "Rails", comments: [comment] },
+      post.deconstruct_keys(nil)
+    )
   end
 
   test "relation" do
